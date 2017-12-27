1ed3c9891036d218f7d8f4e72d3b1382dad21adb
wangliang <1991wangliang@gmail.com>
2017-12-27 16:11:57 +0800

    pom 调整

---
+++ filter-token/src/main/java/com/codingapi/filter/utils/NoReturnConfigUtils.java
@@ -0,0 +1,33 @@
+package com.codingapi.filter.utils;
+
+import com.lorne.core.framework.utils.config.ConfigUtils;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Created by houcunlu on 2017/8/4.
+ */
+public class NoReturnConfigUtils {
+
+    /**
+     *  无需任何数据封装的 地址
+     */
+    private  static List<String> NoReturn = null;
+
+
+    static {
+
+        NoReturn = new ArrayList<>();
+        String [] noreturn  =  ConfigUtils.getStringArrayValue("NoReturn.properties","NoReturn");
+        NoReturn = Arrays.asList(noreturn);
+
+    }
+
+
+    public static List<String> getNoReturn() {
+        return NoReturn;
+    }
+
+}
