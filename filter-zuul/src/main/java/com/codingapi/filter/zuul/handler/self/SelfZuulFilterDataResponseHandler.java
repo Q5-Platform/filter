7f7778f075f003eceb19b0d87f0095d62406f490
wangliang <1991wangliang@gmail.com>
2018-01-06 15:05:36 +0800

    异常处理

--- filter-zuul/src/main/java/com/codingapi/filter/zuul/handler/self/SelfZuulFilterDataResponseHandler.java
+++ filter-zuul/src/main/java/com/codingapi/filter/zuul/handler/self/SelfZuulFilterDataResponseHandler.java
@@ -1,7 +1,5 @@
 package com.codingapi.filter.zuul.handler.self;
 
-import com.alibaba.fastjson.JSONObject;
-import com.codingapi.filter.core.Constants;
 import com.codingapi.filter.core.annotation.FilterResponse;
 import com.codingapi.filter.core.interceptor.handler.FilterDataResponseHandler;
 import com.codingapi.filter.zuul.model.Msg;
@@ -12,7 +10,6 @@ import org.springframework.core.MethodParameter;
 import org.springframework.http.MediaType;
 import org.springframework.http.server.ServerHttpRequest;
 import org.springframework.http.server.ServerHttpResponse;
-import org.springframework.http.server.ServletServerHttpResponse;
 
 /**
  * create by lorne on 2017/12/27
@@ -48,23 +45,14 @@ public class SelfZuulFilterDataResponseHandler implements FilterDataResponseHand
 
         Response res = new Response();
         Msg msg = new Msg();
-        Object jsonObject = null;
-        try {
-            jsonObject = JSONObject.toJSON(body);
-        } catch (Exception e) {
-            jsonObject = body;
-        }
         res.setCode(40000);
-        res.setData(jsonObject);
+        res.setData(body);
         res.setMsg("");
 
         msg.setState(1);
         msg.setRes(res);
 
-        logger.debug("beforeBodyWrite - >"+jsonObject);
-        ((ServletServerHttpResponse) response).getServletResponse().addHeader(Constants.responseHeader,msg.toJsonString());
-
-        return body;
+        return msg;
     }
 
 }
--- filter-zuul/src/main/java/com/codingapi/filter/zuul/handler/self/SelfZuulFilterExceptionHandler.java
+++ filter-zuul/src/main/java/com/codingapi/filter/zuul/handler/self/SelfZuulFilterExceptionHandler.java
@@ -6,7 +6,6 @@ import com.codingapi.filter.zuul.model.Msg;
 import com.codingapi.filter.zuul.model.Response;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.web.bind.annotation.ExceptionHandler;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -20,9 +19,10 @@ public class SelfZuulFilterExceptionHandler implements FilterExceptionHandler{
     private static Logger logger = LoggerFactory.getLogger(SelfZuulFilterExceptionHandler.class);
 
 
-    @ExceptionHandler(value = Exception.class)
-    public void   exceptionHandler(HttpServletRequest request, HttpServletResponse response,
+    public void  exceptionHandler(HttpServletRequest request, HttpServletResponse response,
                                   Object handler, Exception e) throws Throwable {
+
+        e.printStackTrace();
 
         logger.debug("exceptionHandler->" + e + ",handler->" + handler);
         logger.debug("getAttribute -> " + request.getAttribute(Constants.defaultResponseHeader));
